1c11cb8948119f84263effdcb8380c99777725f5
oramadn <111233764+oramadn@users.noreply.github.com>
2024-12-08 17:40:35 +0300

    EXP-28 refactored budget cards into a componenet. Fixed some layouts (#6)

--- src/components/buttons/FilterButtons.tsx
+++ src/components/buttons/FilterButtons.tsx
@@ -94,11 +94,12 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+    paddingHorizontal: 2
   },
   buttonContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    paddingHorizontal: 10
+    paddingHorizontal: 6
   },
 });
 
